09ddb5393702b29fb02cd782464661e514abf90e
Sakshat Kumar <sakshat@settlemintindia.com>
2026-06-18 23:53:06 +0530

    Add AppConstants class for application-wide constant values

---
+++ src/main/java/com/insightflow/constants/AppConstants.java
@@ -0,0 +1,15 @@
+package com.insightflow.constants;
+
+public final class AppConstants {
+
+    private AppConstants() {}
+
+    public static final int DEFAULT_PAGE = 0;
+    public static final int DEFAULT_SIZE = 10;
+    public static final int MAX_PAGE_SIZE = 100;
+    public static final String DEFAULT_SORT_BY = "createdAt";
+    public static final String DEFAULT_SORT_DIR = "desc";
+
+    public static final String TOKEN_PREFIX = "trk_";
+    public static final int TRACKING_KEY_LENGTH = 32;
+}
