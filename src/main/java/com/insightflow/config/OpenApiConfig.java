635b4116c612d81c319a3d1e841409a8a3eaf2c7
Sakshat Kumar <sakshat@settlemintindia.com>
2026-06-18 23:52:32 +0530

    Add OpenApiConfig class for API documentation and security configuration

---
+++ src/main/java/com/insightflow/config/OpenApiConfig.java
@@ -0,0 +1,41 @@
+package com.insightflow.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("InsightFlow Analytics API")
+                        .description("Production-grade SaaS analytics backend API")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("InsightFlow Team")
+                                .email("support@insightflow.com"))
+                        .license(new License()
+                                .name("Proprietary")
+                                .url("https://insightflow.com/license")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Provide a valid JWT access token")));
+    }
+}
