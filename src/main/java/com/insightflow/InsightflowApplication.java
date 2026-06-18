c17123c46b1202f0096ea4f20b629773429a00b2
Sakshat Kumar <sakshat@settlemintindia.com>
2026-06-18 23:51:55 +0530

    Remove unused imports and commented-out CommandLineRunner in InsightflowApplication

--- src/main/java/com/insightflow/InsightflowApplication.java
+++ src/main/java/com/insightflow/InsightflowApplication.java
@@ -1,23 +1,14 @@
 package com.insightflow;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.insightflow.service.ProjectService;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class InsightflowApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InsightflowApplication.class, args);
 	}
-
-	// @Bean
-	// CommandLineRunner run(ProjectService projectService) {
-	// 	return args -> {
-	// 		projectService.createProject();
-	// 	};
-	// }
 }
